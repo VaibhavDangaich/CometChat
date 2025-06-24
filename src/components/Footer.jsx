650b4d6b73dcdaf2e9f916acc33b6285231358ac
VaibhavDangaich <31512333+VaibhavDangaich@users.noreply.github.com>
2025-06-24 16:16:56 +0530

    feat: Enhance Footer component with detailed information
    This commit significantly enhances the Footer component by adding detailed information about the platform, including features, implementation details, and supported technologies.  The new footer provides a more comprehensive overview of the platform's capabilities and integrations.  Specific improvements include structured sections for Features, Implementation, and Technologies, each with multiple sub-items for clarity.  The visual layout is also improved for better readability on larger screens.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,67 @@ import logo from '../assets/logo.png'
 function Footer() {
   return (
       <div className='bg-[#0A0914] lg:pt-[56px] lg:pb-[40px] '>
-          <img src={logo} className='lg:px-[64px]'></img>
-          <div className='lg:px-[64px]'>
-              <div className='flex flex-col'></div>
-              <div></div>
-              <div></div>
-              <div></div>
+          <div className='flex flex-col items-left justify-center lg:gap-[56px]'>
+              <div className='min-w-[1440px]'>
+                  <img src={logo} className='lg:px-[64px] object-cover '></img>
+
+              </div>
+              
+              <div className='lg:px-[64px]'>
+                  <div className='flex flex-col lg:gap-[16px]'>
+                      <h1 className='lg:text-[16px] leading-[112.00000000000001%] tracking-[0.5%] text-[#6852D6]'>Platform</h1>
+                      <div className='flex flex-col lg:gap-[32px]'>
+                          <div className='flex flex-col lg:gap-[16px]'>
+                              <h1 className='opacity-[0.54] lg:text-[14px] text-[#FAFAFF] leading-[140%] tracking-[2%]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Features</h1>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Chat & Messaging</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Voice & Video Calls</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Security & Compliance </p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Extensions</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Features at a glance </p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Webhooks & Bots</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Moderation</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Analytics & Insights</p>
+
+                          </div>
+                          <div className='flex flex-col lg:gap-[16px]'>
+                              <h1 className='opacity-[0.54] lg:text-[14px] text-[#FAFAFF] leading-[140%] tracking-[2%]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Implementation</h1>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Widgets</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>UI Kits</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>SDKs & APIs </p>
+                              
+                          </div>
+                          <div className='flex flex-col lg:gap-[16px]'>
+                              <h1 className='opacity-[0.54] lg:text-[14px] text-[#FAFAFF] leading-[140%] tracking-[2%]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Technologies</h1>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>React Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Angular Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Vue Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>IOS Swift Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Android Kotlin Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Android Java Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>React Native Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Ionic/Capacitor Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>WordPress Chat SDK & API </p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Lavarel/PHP Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Flutter Chat SDK & API</p>
+                              <p className='opacity-[0.84] lg:text-[14px] leading-[140%] tracking-[2%] text-[#FAFAFF]' style={{ fontFamily: 'SatoshiVariable', fontWeight: 600 }}>Next.js Chat SDK & API</p>
+
+                              
+                          </div>
+                          
+                      </div>
+                      
+                  </div>
+                  <div className='flex flex-col lg:gap-[16px]'>
+                      
+                  </div>
+                  <div></div>
+                  <div></div>
+
+        
+              </div>
               
           </div>
+          
     </div>
   )
 }
